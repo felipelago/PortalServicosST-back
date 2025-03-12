7fee4552747c24602eb17a37e917669247d8f7d3
Felipe <felipelagocoimbra@hotmail.com>
2025-03-12 00:27:19 -0300

    configs

---
+++ src/main/java/br/com/portalservicos/portal_servicos_st/PortalServicosStApplication.java
@@ -0,0 +1,13 @@
+package br.com.portalservicos.portal_servicos_st;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalServicosStApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalServicosStApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/portalservicos/portal_servicos_st/PortalServicosStApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.portalservicos.portal_servicos_st;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalServicosStApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
